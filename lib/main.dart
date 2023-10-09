fadc4da4b649add49f0443279607908f6dae6cd2
mahmoud hassan <mahmoudhs236@gmail.com>
2023-10-09 16:56:48 +0300

    create todo app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'ToDo App',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
